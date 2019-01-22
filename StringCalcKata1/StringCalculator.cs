74b94b8770ba75c4a62a57a27b9728f482eafb32
Matt Horrall <mhorrall@gmail.com>
2019-01-22 17:11:09 -0600

    All tests pass

---
+++ StringCalcKata1/StringCalculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalcKata1
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            var sum = 0;
+            var delimiters = new List<string> {",", "\n"};
+
+            if (string.IsNullOrWhiteSpace(numbers)) return sum;
+
+            if (numbers.StartsWith("//"))
+            {
+                delimiters.AddRange(FindDelimiters(numbers));
+                numbers = RemoveDelimIndicatorStrings(numbers, new List<string>{"//", "[", "]"});
+            }
+
+            var integers = numbers.Split(delimiters.ToArray(),
+                StringSplitOptions.RemoveEmptyEntries).Select(s => Convert.ToInt32(s));
+
+            var negativeList = integers.Where(i => i < 0);
+
+            if (negativeList.Any())
+            {
+                throw new ArgumentException($"Negatives not allowed: {string.Join(",", negativeList.ToArray())}");
+            }
+
+            return integers
+                .Where(i => i <= 1000)
+                .Sum();
+        }
+
+        private string RemoveDelimIndicatorStrings(string numbers, List<string> delimIndicatorList)
+        {
+            foreach (var delimiterIndicator in delimIndicatorList)
+            {
+                numbers = numbers.Replace(delimiterIndicator, "");
+            }
+
+            return numbers;
+        }
+
+        private List<string> FindDelimiters(string numbers)
+        {
+            var results = new List<string>();
+
+            if (numbers.Contains("[") && numbers.Contains("]"))
+            {
+                var matches = Regex.Matches(numbers, @"\[(.*?)\]");
+                foreach (Match match in matches)
+                {
+                    results.Add(match.Groups[1].Value);
+                }
+            }
+            else
+            {
+                results.Add(numbers.Substring(2, 1));
+            }
+
+            return results;
+        }
+    }
+}
---
+++ StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringCalcKata1;
+using Xunit;
+
+namespace StringCalculatorTests
+{
+    public class StringCalculatorAdd
+    {
+        private readonly StringCalculator _stringCalculator = new StringCalculator();
+
+        [Fact]
+        public void EmptyStringReturns0()
+        {
+            var result = _stringCalculator.Add("");
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void OneNumberReturnsOne()
+        {
+            var result = _stringCalculator.Add("1");
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void OnePlusTwoReturnsThree()
+        {
+            var result = _stringCalculator.Add("1,2");
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void OneWithNewLinePlusTwoPlusThreeReturnsSix()
+        {
+            var result = _stringCalculator.Add("1\n2,3");
+
+            Assert.Equal(6, result);
+        }
+
+        [Theory]
+        [InlineData("-1,2")]
+        public void OneNegativeThrowsException(string input)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => _stringCalculator.Add(input));
+
+            Assert.Equal("Negatives not allowed: -1", ex.Message);
+        }
+
+        [Theory]
+        [InlineData("2,-4,3,-5")]
+        public void MultipleNegativeThrowsException(string input)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => _stringCalculator.Add(input));
+
+            Assert.Equal("Negatives not allowed: -4,-5", ex.Message);
+        }
+
+        [Fact]
+        public void IgnoreGreaterThan1000()
+        {
+            // eg. "1001,2" returns 2
+            var result = _stringCalculator.Add("1001,2");
+
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void DelimiterAnyLength()
+        {
+            var result = _stringCalculator.Add("//[|||]\n1|||2|||3");
+
+            Assert.Equal(6, result);
+        }
+
+        [Theory]
+        [InlineData("//[|][%]\n1|2%3")]
+        [InlineData("//[|||][%%]\n1|||2%%3")]
+        public void MultipleDelimiters(string input)
+        {
+            var result = _stringCalculator.Add(input);
+
+            Assert.Equal(6, result);
+        }
+    }
+}
